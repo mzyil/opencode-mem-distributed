4cec641f1602ef1e25a7e6d7e43f94912486c0ae
Melih Yildiz <melih.yildiz@banxware.com>
2026-05-20 15:58:32 +0200

    chore: scaffold storage-abstraction branch with no-floating-promises lint

---
+++ eslint.config.js
@@ -0,0 +1,16 @@
+import tseslint from "@typescript-eslint/eslint-plugin";
+import tsparser from "@typescript-eslint/parser";
+
+export default [
+  {
+    files: ["src/services/storage/**/*.ts"],
+    languageOptions: {
+      parser: tsparser,
+      parserOptions: { project: "./tsconfig.json" },
+    },
+    plugins: { "@typescript-eslint": tseslint },
+    rules: {
+      "@typescript-eslint/no-floating-promises": "error",
+    },
+  },
+];
